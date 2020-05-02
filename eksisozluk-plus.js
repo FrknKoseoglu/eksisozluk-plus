103963281d485f1ae170f1d09359bbeca1565100
Furkan Köseoğlu <furkan1771@gmail.com>
2020-05-02 12:03:45 +0300

    - jQuery eklendi. - Ana sayfa > Sponsor Entry Kaldırıldı. - Genel Basit reklam önlemi.

--- eksisozluk-plus.js
+++ eksisozluk-plus.js
@@ -1,15 +1,27 @@
 // ==UserScript==
 // @name         Ekşisözlük+
 // @namespace    https://github.com/FrknKoseoglu/eksisozluk-plus
-// @version      0.1
+// @version      0.2
 // @description  Daha iyi bir ekşisözlük deneyimi
 // @author       Furkan Köseoğlu
 // @match        https://eksisozluk.com/
 // @grant        none
 // ==/UserScript==
 
+var $ = window.jQuery;
+
 (function() {
     'use strict';
     document.getElementById("search-textbox").maxLength = 50; // Arama Çubuğu 50 Karakter Sınırlama
 
 })();
+
+$(document).ready(function() {
+    //Genel basit reklam kaldırma
+    $(".ad-double-click").css('display', 'none');
+
+    // Sponsor içerik kaldırma
+    var sponsor = $("#corporate-content-sponsored-entry").parent();
+    $(sponsor).parent().css('display', 'none');
+
+});
